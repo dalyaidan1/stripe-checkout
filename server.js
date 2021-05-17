814d68c269d55b9bc03952d7cd76d75f53d5f15b
Aidan Daly <dalyaidan1@gmail.com>
2021-05-17 17:32:25 -0400

    ejs templating for store items

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ if (process.env.NODE_ENV !== "production") {
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY
 const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
 
-console.log(stripeSecretKey, stripePublicKey)
+// console.log(stripeSecretKey, stripePublicKey)
 
 const express = require("express")
 const app = express() // express saves all the manual setup from a basic node server
-const fs = require("fr") // read different files
+const fs = require("fs") // read different files
 
 app.set("view engine", "ejs") // allow embedding server side code to html
 app.use(express.static("public")) // all file in public are static, and available on front-end
