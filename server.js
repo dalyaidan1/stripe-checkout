a1239f0e9c34397869153b48261012da580d73be
Aidan Daly <dalyaidan1@gmail.com>
2021-05-17 17:02:31 -0400

    stripe add and json db

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+// because we only included dotenv lib for dev it wont exist in production so check the NODE_ENV (set by node) if we are in production to avoid errors
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config() // use the env
+}
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
+
+console.log(stripeSecretKey, stripePublicKey)
+
 const express = require("express")
 const app = express() // express saves all the manual setup from a basic node server
 
