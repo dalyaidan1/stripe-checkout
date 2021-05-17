3618df0938f398e969af6c563fa8ec7d773427e5
Aidan Daly <dalyaidan1@gmail.com>
2021-05-17 16:19:29 -0400

    ejs framing

--- server.js
+++ server.js
@@ -1,2 +1,7 @@
 const express = require("express")
 const app = express() // express saves all the manual setup from a basic node server
+
+app.set("view engine", "ejs") // allow embedding server side code to html
+app.use(express.static("public")) // all file in public are static, and available on front-end
+
+app.listen(3000)
